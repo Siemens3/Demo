f48cef9c71eed3cf26ac81db033ee7ecc169644f
Siemens3 <steo.kenny@hotmail.co.uk>
2014-10-03 15:43:24 +0100

    comint final

--- week3/script.sh
+++ week3/script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Title :cpucount.sh 
-# Date  : 26/02/14
+# Title :script.sh 
+# Date  : 3/10/14
 # Author  :Stephen kenny
 # Version :1.0
-# Description : counts the number of cpus on guest machine
+# Description : counts the number of cpus on guest machine and prints nice display
 # Options  
 
 cpucnt=$(grep 'processor' /proc/cpuinfo | wc -l)
